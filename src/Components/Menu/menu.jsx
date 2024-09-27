d5472ceba9a30750e7a2f744e4719705b81fbcfd
Harshit Pathak <128951265+HarshitPathak-02@users.noreply.github.com>
2024-09-27 13:40:01 +0530

    Update menu.jsx

--- src/Components/Menu/menu.jsx
+++ src/Components/Menu/menu.jsx
@@ -4,30 +4,6 @@ import { AiOutlineClose } from 'react-icons/ai'
 
 const menu = () => {
   return (
-    let menu_container = document.querySelector(".menu_container")
-    let skills_head = document.querySelector("#skills_head")
-    let project_head= document.querySelector("#projects_head")
-    let contact_head = document.querySelector("#contact_head")
-
-    skills_head.addEventListener("click",()=>{
-      gsap.to(menu_container,{
-        display:'none',
-        duration:0.5
-      })
-    })
-    contact_head.addEventListener("click",()=>{
-      gsap.to(menu_container,{
-        display:'none',
-        duration:0.5
-      })
-    })
-    project_head.addEventListener("click",()=>{
-      gsap.to(menu_container,{
-        display:'none',
-        duration:0.5
-      })
-    })
-    
     <div className='menu_container'>
       {/* <div className="close">
         <h4 style={{color:'white'}} className='close_h4'><AiOutlineClose id='close'/></h4>
